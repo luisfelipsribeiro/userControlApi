8c5544fb3a612293b97d90c190f178a9b24449e2
luis.moreira <luis.moreira@cmsw.com>
2022-06-02 12:44:54 -0300

    crud creation and url params handling

---
+++ src/mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Luis",
+  },
+  {
+    id: 2,
+    name: "Felipe",
+  },
+  {
+    id: 3,
+    name: "Pedro",
+  },
+  {
+    id: 4,
+    name: "Henrique",
+  },
+];
